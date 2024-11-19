2ab9e408bd7de06d35c0663a892568a283b5b6e8
AayushTyagi1 <tyagiaayush5@gmail.com>
2024-11-20 01:26:47 +0530

    Fix Videos and Image

--- main.py
+++ main.py
@@ -33,25 +33,6 @@ footer {{ visibility: hidden; }}
 """, unsafe_allow_html=True)
 
 
-def format_message(text):
-    """
-    This function is used to format the messages in the chatbot UI.
-
-    Parameters:
-    text (str): The text to be formatted.
-    """
-    text_blocks = re.split(r"```[\s\S]*?```", text)
-    code_blocks = re.findall(r"```([\s\S]*?)```", text)
-
-    text_blocks = [html.escape(block) for block in text_blocks]
-
-    formatted_text = ""
-    for i in range(len(text_blocks)):
-        formatted_text += text_blocks[i].replace("\n", "<br>")
-        if i < len(code_blocks):
-            formatted_text += f'<pre style="white-space: pre-wrap; word-wrap: break-word;"><code>{html.escape(code_blocks[i])}</code></pre>'
-    return formatted_text
-
 def display_message(text, is_user=False, avatar_url=None):
     """Displays a chat message with custom styling and avatar."""
     avatar_style = "width:40px; height:40px; margin:0;"
@@ -61,12 +42,12 @@ def display_message(text, is_user=False, avatar_url=None):
     alignment = "flex-end" if is_user else "flex-start"
     margin = "margin-left" if is_user else "margin-right"
 
-    formatted_text = format_message(text.strip())
+    formatted_text = text.strip()
 
     if is_user:
         container_html = f"""
         <div style="display:flex; align-items:flex-start; justify-content:{alignment}; margin-bottom:10px;">
-            <div style="background:{message_bg_color}; color:white; border-radius:20px; padding:10px; {margin}:5px; max-width:75%; font-size:14px; line-height:1.2; word-wrap:break-word;">
+            <div style="background:{message_bg_color}; color:white; border-radius:20px; padding:10px; {margin}:5px; max-width:90%; font-size:14px; line-height:1.2; word-wrap:break-word;">
                 {formatted_text}
             </div>
         </div>
@@ -76,7 +57,7 @@ def display_message(text, is_user=False, avatar_url=None):
         container_html = f"""
         <div style="display:flex; align-items:flex-start; justify-content:{alignment}; margin-bottom:10px;">
             <img src="{avatar_url}" alt="avatar" style="{avatar_style}; margin-right:5px;" />
-            <div style="background:{message_bg_color}; color:white; border-radius:20px; padding:10px; max-width:75%; font-size:14px; line-height:1.2; word-wrap:break-word;">
+            <div style="background:{message_bg_color}; color:white; border-radius:20px; padding:10px; max-width:90%; font-size:14px; line-height:1.2; word-wrap:break-word;">
                 {formatted_text}
                 <div style="font-size:12px; color:#ccc; margin-top:5px;">{current_time}</div>
             </div>
